01e40d7b12004ebfd3a73e23f8aed1cb8d2fa0e8
Vitalii111 <32221560+Vitalii111@users.noreply.github.com>
2023-01-17 00:38:37 +0200

    comm

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 const textREf = document.querySelector("#text");
 const inputRef = document.querySelector("#font-size-control");
 // textREf.style.fontSize = "56px";
-inputRef.value = 16;
+//inputRef.value = 16;
+textREf.style.fontSize = inputRef.value + "px";
 inputRef.addEventListener("input", onInputChange);
 function onInputChange(event) {
   textREf.style.fontSize = event.target.value + "px";
